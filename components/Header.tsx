e4d13bc47090511edadf8c25043417f6aa0c4dc5
Xavier Oliveira <2160795@my.ipleiria.pt>
2023-01-12 22:02:52 +0000

    add images to the Header and cleanup global css

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
 import { Tabs } from '../pages/_app';
+import styles from '../styles/Header.module.css'
+import Image from 'next/image'
 
 interface HeaderProps {
   tabSelected: Tabs
@@ -8,10 +10,46 @@ interface HeaderProps {
 const Header = ({ tabSelected, setTabSelected }: HeaderProps) => {
   return (
     <>
-      <button onClick={() => setTabSelected(Tabs.INFORMACAO)} >Button 1</button>
-      <button onClick={() => setTabSelected(Tabs.RESPOSTA)}>Button 2</button>
-      {tabSelected}
-      <p style={{ fontFamily: 'Olivia-Regular'}}>VAMOS casar</p>
+      <div className={styles.header}>
+        <Image
+          src="/background_image.svg"
+          alt=""
+          fill
+          className={styles.backgroundImage}
+          // sizes="(max-width: 1200px) 100vw, 90vw"
+          priority
+          />
+        <div className={styles.logo}>
+          <Image
+            src="/logo.svg"
+            alt="XDLogo"
+            width={350}
+            height={350}
+            priority
+          />
+          <div className={styles.textHeaderRoot}>
+            <p className={styles.textHeader}>
+              Vamos Casar
+              <Image
+                src="/rings.svg"
+                alt="rings"
+                width={77}
+                height={64}
+                priority
+                color="--gold-color"
+              />
+            </p>
+            <p>
+              Bem-vindos!!
+            </p>
+            <p>
+              Estamos ansiosos para partilhar este momento especial na nossa vida com todos vocês e esperamos que possam celebrar comnosco este dia memorável
+            </p>
+          </div>
+        </div>
+        <button onClick={() => setTabSelected(Tabs.INFORMACAO)} >Button 1</button>
+        <button onClick={() => setTabSelected(Tabs.RESPOSTA)}>Button 2</button>
+      </div>
     </>
   );
 };
