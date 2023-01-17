7b8747377eb3ab71cb2e148702933992d0a107c4
Xavier Oliveira <2160795@my.ipleiria.pt>
2023-01-17 19:35:04 +0000

    fix pipeline by adding a key prop on an array

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import Card from '../components/Card';
 import styles from '../styles/Card.module.css';
 
-const CardData = [{ title: 'A data', subTitle: '20 de Maio de 2023', description: 'Vamos casar-nos no sábado, dia 20 de Maio, e esperamos que possas celebrar connosco este momento tão importante das nossas vidas.'}];
+const CardData = [
+  {
+    title: 'A data',
+    subTitle: '20 de Maio de 2023',
+    description: 'Vamos casar-nos no sábado, dia 20 de Maio, e esperamos que possas celebrar connosco este momento tão importante das nossas vidas.'
+  },
+];
 
 
 export default function Home() {
   return (
     <div className={styles.bodyCards}>
         {
-            CardData.map(({ title, subTitle, description }) => {
-                return <Card title={title} subTitle={subTitle} description={description} />
+            CardData.map(({ title, subTitle, description }, index) => {
+                return <Card key={`${index}-${title}`} title={title} subTitle={subTitle} description={description} />
             })
         }
     </div>
