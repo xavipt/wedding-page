7ad5c1912999b7ba44e24740b64a87f9c8d3d7bd
Débora <d.antunes@kigroup.de>
2023-02-03 12:17:13 +0000

    feat(card): add card image; add border-radius

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,19 @@ const CardData = [
     title: 'A data',
     subTitle: '20 de Maio de 2023',
     description: 'Vamos casar-nos no sábado, dia 20 de Maio, e esperamos que possas celebrar connosco este momento tão importante das nossas vidas.',
-    image: '/party.png'
+    image: '/us.jpg'
   },
   {
     title: 'Cerimónia',
     subTitle: 'Igreja de Santa Eunfémia, 12h',
     description: 'O ponto de encontro vai ser na Igreja de Sta. Eufémia, onde irá ser realizado o casamento às 12h.',
-    image: '/church.png'
+    image: '/church.jpg'
   },
   {
     title: 'Festa',
     subTitle: 'Quinta do Ti Lucas',
     description: 'A festa continua com o almoço na Quinta do Ti Lucas, onde não irá faltar comida, diversão e boa disposição.',
-    image: '/party.png'
+    image: '/party.jpg'
   }
 ];
 
